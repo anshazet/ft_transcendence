dcf3a01c1892a758a454a632948d402c8bc779f7
Olskor <jbamine34@gmail.com>
2024-05-07 15:30:22 +0200

    réarangement

---
+++ makefile
@@ -0,0 +1,25 @@
+NAME = transcendance
+COMPOSE_FILE = ./docker-compose.yml
+
+all: build run
+
+build:
+	sudo docker-compose -f $(COMPOSE_FILE) build
+	@echo "\n\ntranscendance is ready to be launched."
+
+run:
+	sudo docker-compose -f $(COMPOSE_FILE) up -d
+	@echo "\n\ntranscendance is now running."
+
+stop:
+	sudo docker-compose -f $(COMPOSE_FILE) stop
+
+clean: stop
+	sudo docker-compose -f $(COMPOSE_FILE) down -v
+
+fclean: clean
+	@if [ -n "$$(sudo docker ps -a -q)" ]; then sudo docker rm -f $$(sudo docker ps -a -q); fi
+	@if [ -n "$$(sudo docker images -q)" ]; then sudo docker rmi -f $$(sudo docker images -q); fi
+	@if [ -n "$$(sudo docker volume ls -q)" ]; then sudo docker volume prune -f; fi
+
+re: fclean all
---
+++ static/js/account.js
@@ -0,0 +1,68 @@
+document.getElementById('register-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const formData = new FormData(this);
+    const csrftoken = getCookie('csrftoken');
+    fetch('/register/', {
+        method: 'POST',
+        body: formData
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.success) {
+            console.log(data.success);
+        } else {
+            console.error(data.error);
+        }
+    })
+    .catch(error => {
+        console.error('Erreur lors de la requête:', error);
+    });
+});
+
+document.getElementById('login-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const username = document.getElementById('login-username').value;
+    const password = document.getElementById('login-password').value;
+
+    fetch('/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'X-CSRFToken': getCookie('csrftoken')
+        },
+        body: `username=${encodeURIComponent(username)}&password=${encodeURIComponent(password)}`
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.success) {
+            const modal = document.getElementById('ModalForm');
+            const modalInstance = bootstrap.Modal.getInstance(modal);
+            modalInstance.hide();
+            console.log(data.success);
+        } else {
+            console.error(data.error);
+        }
+    })
+    .catch(error => {
+        console.error('Erreur lors de la requête:', error);
+    });
+});
+
+
+
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+const csrftoken = getCookie('csrftoken');
