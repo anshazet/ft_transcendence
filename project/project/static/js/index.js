e6d0042e279eca7f7c412b9fed2460d5081e248b
Benjoplacop <bhennequ@BenPC>
2024-05-07 15:21:40 +0200

    add new user space

---
+++ project/project/static/js/index.js
@@ -0,0 +1,35 @@
+let currentSection = null;
+
+function showSection(sectionId) {
+    const current = document.getElementById('section-' + currentSection);
+    const target = document.getElementById('section-' + sectionId);
+    
+    if (current) {
+        current.style.display = 'none';
+    }
+    target.style.display = 'block';
+    currentSection = sectionId;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelectorAll('.nav-link').forEach(function(anchor) {
+        anchor.addEventListener('click', function(event) {
+            event.preventDefault(); 
+            const targetId = this.getAttribute('href').substring(1);
+            showSection(targetId);
+        });
+    });
+    showSection('pong');
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const btnOpenInscription = document.getElementById('btn-open-inscription');
+    const sectionLogin = document.getElementById('ModalForm');
+    const sectionInscription = document.getElementById('ModalFormRegister');
+
+    btnOpenInscription.addEventListener('click', function() {
+        sectionLogin.style.display = 'none';
+        sectionInscription.style.display = 'block';
+
+    });
+});
