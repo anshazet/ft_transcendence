53880c46f832e35ac379777eec1f517efeaaa157
Olskor <jbamine34@gmail.com>
2024-05-07 16:48:40 +0200

    pt

--- project/project/project/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class Utilisateur(models.Model):
-    username = models.CharField(max_length=100)
-    password = models.CharField(max_length=100)
-    email = models.CharField(max_length=100)
