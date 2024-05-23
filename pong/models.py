921d6cfc4cd0c9dd70f3947686878881902a3fe7
Benjoplacop <bhennequ@BenPC>
2024-05-23 16:23:36 +0200

    add avatar

--- pong/models.py
+++ pong/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 import os
 
-class Utilisateur(models.Model):
-    username = models.CharField(max_length=100)
-    password = models.CharField(max_length=100)
-    email = models.CharField(max_length=100)
+# class Utilisateur(models.Model):
+#     username = models.CharField(max_length=100)
+#     password = models.CharField(max_length=100)
+#     email = models.CharField(max_length=100)
+
+
 
 # from django.contrib.auth.models import User
 # from django.db import models
@@ -15,3 +17,26 @@ class Utilisateur(models.Model):
 
 #     class Meta:
 #         db_table = 'pong_utilisateur'
+
+from django.contrib.auth.models import User
+from django.db import models
+
+class FriendRequest(models.Model):
+    from_user = models.ForeignKey(User, related_name='sent_friend_requests', on_delete=models.CASCADE)
+    to_user = models.ForeignKey(User, related_name='received_friend_requests', on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.from_user} -> {self.to_user}"
+    
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    friends = models.ManyToManyField('self', through='Friendship', symmetrical=False)
+
+class Friendship(models.Model):
+    from_user = models.ForeignKey(User, related_name='friendship_creator_set', on_delete=models.CASCADE)
+    to_user = models.ForeignKey(User, related_name='friend_set', on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.from_user} -> {self.to_user}"
